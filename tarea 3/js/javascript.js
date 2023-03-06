5f3f4e25665f26f6f5ce83897e3361f2a154ea66
tomisonrix <124640136+tomisonrix@users.noreply.github.com>
2023-03-05 22:25:03 -0300

    Add files via upload

---
+++ tarea 3/js/javascript.js
@@ -0,0 +1,7 @@
+let edad = prompt ("Ingrese su edad");
+if (parseInt(edad) >=18){
+alert ("Bienvenido a mi sitio web sos mayor")
+}
+else{
+    alert ("Sos menor, ingresaras al sitio web bajo tu responsabilidad")
+}
---
+++ tarea 3/js/tienda.js
@@ -0,0 +1,7 @@
+var opcion = prompt ("¿Usted se encuentra en San Nicolas o en un radio dentro de 40km?")
+if (opcion=="si"){
+    alert("El servicio a domiciolio tiene un costo de envio de $0")
+}
+else{
+    alert("El costo del servicio a domicilio sera de $1300")
+}
